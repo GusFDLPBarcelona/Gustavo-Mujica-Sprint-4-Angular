c7c0f7fdcdbf99c88c91b06135349f1eb9bab167
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-05-19 14:27:35 +0200

    Creación del proyecto. Solución del Ejercicio 1 del Nivel 1 del Sprint 4. Index.html y app.ts. Redacción del archivo READ.me con explicaciones.

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+interface Joke {
+    id: string;
+    joke: string;
+}
+
+const jokeText = document.getElementById('jokeText') as HTMLParagraphElement;
+const nextJokeButton = document.getElementById('nextJokeButton') as HTMLButtonElement;
+
+async function fetchJoke(): Promise<Joke> {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const joke: Joke = await response.json();
+    return joke;
+}
+async function displayJoke(): Promise<void> {
+    try {
+        const joke = await fetchJoke();
+        jokeText.innerText = joke.joke;
+        console.log(joke);
+    } catch (error: any) {
+        jokeText.innerText = 'Error al cargar el chiste';
+        console.error('Error fetching joke:', error);
+    }
+}
+
+nextJokeButton.addEventListener('click', displayJoke);
+
+displayJoke();
